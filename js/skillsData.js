e9da3b8e744359ddc6c94f99f1f1af641219458d
Heriberto Montoya <hmontoya3821@gmail.com>
2021-06-09 13:26:52 -0500

    first commit

---
+++ js/skillsData.js
@@ -0,0 +1,54 @@
+export const skills = [
+  {
+    devicon: '<i class="devicon-css3-plain colored"></i>',
+    skillName: "CSS3",
+  },
+  {
+    devicon: '<i class="devicon-javascript-plain colored"></i>',
+    skillName: "JavaScript",
+  },
+  {
+    devicon: '<i class="devicon-python-plain colored"></i>',
+    skillName: "python",
+  },
+  {
+    devicon: '<i class="devicon-react-original colored"></i>',
+    skillName: "react",
+  },
+  {
+    devicon: '<i class="devicon-redux-original colored"></i>',
+    skillName: "redux",
+  },
+  {
+    devicon: '<i class="devicon-sqlalchemy-original-wordmark"></i>',
+    skillName: "sqlalchemy",
+  },
+  {
+    devicon: '<i class="devicon-flask-original"></i>',
+    skillName: "flask",
+  },
+  {
+    devicon: '<i class="devicon-express-original colored"></i>',
+    skillName: "express",
+  },
+  {
+    devicon: '<i class="devicon-html5-plain colored"></i>',
+    skillName: "html5",
+  },
+  {
+    devicon: '<i class="devicon-amazonwebservices-original colored"></i>',
+    skillName: "amazonwebservices",
+  },
+  {
+    devicon: '<i class="devicon-git-plain colored"></i>',
+    skillName: "git",
+  },
+  {
+    devicon: '<i class="devicon-postgresql-plain colored"></i>',
+    skillName: "postgresql",
+  },
+  {
+    devicon: '<i class="devicon-sequelize-plain colored"></i>',
+    skillName: "sequelize",
+  },
+];
